68d4e68b06e2e0d2821b5ab4283825606fd67e0e
huzaechxx <huzaifazahid87@gmail.com>
2025-05-28 03:07:14 +0500

    Update LowerNav.jsx

--- my-app/src/components/LowerNav.jsx
+++ my-app/src/components/LowerNav.jsx
@@ -239,7 +239,7 @@ const LowerNav = () => {
           px: 1.5,
           color: "white",
           position: "relative",
-          left: "40%",
+          left: "39%",
         }}
       >
         Contact IBM Consulting
